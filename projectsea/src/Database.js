6a0a59397f0c95a45a6b728f8c81122b6ceeaa75
jainjj <jain.joseph@suse.com>
2022-06-29 10:25:57 +0100

    reformat

--- projectsea/src/Database.js
+++ projectsea/src/Database.js
@@ -1,8 +1,9 @@
 const mysql = require("mysql");
 
 const connection = mysql.createPool({
-  host: "http://ec2-18-202-26-166.eu-west-1.compute.amazonaws.com/phpmyadmin/",
+  host: "http://ec2-18-202-26-166.eu-west-1.compute.amazonaws.com",
   port:"3306",
+  path:"/phpmyadmin"
   user: "leaderboard",
   password: "leaderboard",
   database: "leads"
