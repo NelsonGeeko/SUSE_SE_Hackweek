004b4195f2b28ec9668f28594cd208d3cdda12e9
jainjj <jain.joseph@suse.com>
2022-06-29 10:10:28 +0100

    db

---
+++ projectsea/src/Database.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql");
+
+const connection = mysql.createPool({
+  host: "http://ec2-18-202-26-166.eu-west-1.compute.amazonaws.com/phpmyadmin/",
+  user: "leaderboard",
+  password: "leaderboard",
+  database: "leads"
+});
+
+connection.query(
+  "SELECT * FROM leads",
+  (err, result) => {
+    err ? console.log(err) : console.log(result[0].hexcode);
+  }
+);
