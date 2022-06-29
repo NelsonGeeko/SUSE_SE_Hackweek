3766ad02249a83b9556b2d54da85a3f989744e69
NelsonGeeko <nelson.batsford@suse.com>
2022-06-29 12:15:50 +0100

    commit

--- projectsea/src/Dashboard.js
+++ projectsea/src/Dashboard.js
@@ -17,7 +17,7 @@ import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-// import { mainListItems, secondaryListItems } from './listItems';
+import { mainListItems, secondaryListItems } from './ListItems';
 // import Chart from './Chart';
 // import Deposits from './Deposits';
 // import Orders from './Orders';
@@ -141,9 +141,9 @@ function DashboardContent() {
           </Toolbar>
           <Divider />
           <List component="nav">
-            {/* {mainListItems} */}
+            {mainListItems}
             <Divider sx={{ my: 1 }} />
-            {/* {secondaryListItems} */}
+            {secondaryListItems}
           </List>
         </Drawer>
         <Box
