932c26b0958674320f8599e5ac24dc4b20293ddc
jainjj <jain.joseph@suse.com>
2022-06-29 10:28:13 +0100

    update

--- projectsea/src/Database.js
+++ projectsea/src/Database.js
@@ -3,14 +3,14 @@ const mysql = require("mysql");
 const connection = mysql.createPool({
   host: "http://ec2-18-202-26-166.eu-west-1.compute.amazonaws.com",
   port:"3306",
-  path:"/phpmyadmin"
+  path:"/phpmyadmin",
   user: "leaderboard",
   password: "leaderboard",
   database: "leads"
 });
 
 connection.query(
-  "SELECT * FROM leads",
+  "SELECT  FROM leads",
   (err, result) => {
     err ? console.log(err) : console.log(result[0].hexcode);
   }
