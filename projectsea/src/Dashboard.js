c4d568900fc9a0ffd7327ba4bb0ce24aced173a0
jainjj <jain.joseph@suse.com>
2022-06-29 13:48:18 +0100

    orders

--- projectsea/src/Dashboard.js
+++ projectsea/src/Dashboard.js
@@ -82,7 +82,7 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 );
 
 const mdTheme = createTheme();
-
+<Orders></Orders>
 function DashboardContent() {
   const [open, setOpen] = React.useState(true);
   const toggleDrawer = () => {
