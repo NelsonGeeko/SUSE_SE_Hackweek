12e49a8bb452e37a8b6d6df277f72b5dfa8b4309
jainjj <jain.joseph@suse.com>
2022-06-29 10:48:11 +0100

    port added

--- projectsea/src/Database.js
+++ projectsea/src/Database.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "ec2-18-202-26-166.eu-west-1.compute.amazonaws.com/phpmyadmin/",
+  host: "ec2-18-202-26-166.eu-west-1.compute.amazonaws.com/phpmyadmin:3306",
   user: "leaderboard",
   password: "leaderboard"
 });
