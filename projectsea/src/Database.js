8b8f0d8b58e403f911000b47148506c3dbdcfbb3
jainjj <jain.joseph@suse.com>
2022-06-29 10:49:13 +0100

    port

--- projectsea/src/Database.js
+++ projectsea/src/Database.js
@@ -1,7 +1,8 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "ec2-18-202-26-166.eu-west-1.compute.amazonaws.com/phpmyadmin:3306",
+  host: "ec2-18-202-26-166.eu-west-1.compute.amazonaws.com/phpmyadmin",
+  port:"3306",
   user: "leaderboard",
   password: "leaderboard"
 });
