c328acb586767c94dfc8d142336366e142b27910
jainjj <jain.joseph@suse.com>
2022-06-29 10:53:08 +0100

    ,

--- projectsea/src/Database.js
+++ projectsea/src/Database.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "ec2-18-202-26-166.eu-west-1.compute.amazonaws.com",
   port:"3306",
-  path:"/phpmyadmin"
+  path:"/phpmyadmin",
   user: "leaderboard",
   password: "leaderboard"
 });
